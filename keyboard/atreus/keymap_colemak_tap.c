4823bcb304098d7f4606b51c0d75c485971744ea
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-07-21 21:57:06 -0400

    trying out yet another symbol layout

--- keyboard/atreus/keymap_colemak_tap.c
+++ keyboard/atreus/keymap_colemak_tap.c
@@ -11,24 +11,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                        KC_K,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, \
      KC_ESC,   KC_TAB,   KC_LGUI,  KC_FN1,   KC_FN6,  KC_FN3,   KC_FN4,   KC_FN5,   KC_FN0,  KC_MINS,  KC_QUOT,  KC_ENT), \
 
-  // 1: punctuation and numbers *=+
+  // 1: punctuation and numbers
+
+  // *7890  |[]\!
+  // +456`  @()^$
+  // =123~  &{}%#
+  //  0.      -'
+
   KEYMAP(
-    SHIFT(KC_8),   KC_7,    KC_8,    KC_9,    KC_0,            SHIFT(KC_BSLS), KC_LBRC,        KC_RBRC,        SHIFT(KC_6), SHIFT(KC_4),\
-    SHIFT(KC_EQL), KC_4,    KC_5,    KC_6,    KC_GRAVE,        KC_BSLS,        SHIFT(KC_9),    SHIFT(KC_0),    SHIFT(KC_5), SHIFT(KC_7),\
-    KC_EQL,        KC_1,    KC_2,    KC_3,    SHIFT(KC_GRAVE), SHIFT(KC_2),    SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_3), SHIFT(KC_1),\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS), \
+    SHIFT(KC_8),   KC_7,    KC_8,    KC_9,    KC_0,            SHIFT(KC_BSLS), KC_LBRC,        KC_RBRC,        KC_BSLS,     SHIFT(KC_1),\
+    SHIFT(KC_EQL), KC_4,    KC_5,    KC_6,    KC_GRAVE,        SHIFT(KC_2),    SHIFT(KC_9),    SHIFT(KC_0),    SHIFT(KC_6), SHIFT(KC_4),\
+    KC_EQL,        KC_1,    KC_2,    KC_3,    SHIFT(KC_GRAVE), SHIFT(KC_7),    SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_5), SHIFT(KC_3),\
+    KC_TRNS,       KC_0,    KC_DOT,  KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,     KC_TRNS), \
 
   // 2: arrows and function keys
   KEYMAP(
-     KC_INS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,                       KC_HOME,  KC_RGHT,  KC_END,   KC_PGUP,  KC_NO, \
-     KC_DEL,   KC_F4,    KC_F5,    KC_F6,    KC_F11,                       KC_LEFT,  KC_DOWN,  KC_UP,    KC_PGDN,  KC_NO, \
-     KC_NO,    KC_F1,    KC_F2,    KC_F3,    KC_F12,                       KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, \
+     KC_INS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,                       KC_HOME,  KC_RGHT,  KC_END,   KC_PGUP,  KC_PSCR, \
+     KC_DEL,   KC_F4,    KC_F5,    KC_F6,    KC_F11,                       KC_LEFT,  KC_DOWN,  KC_UP,    KC_PGDN,  KC_PAUSE, \
+     KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F12,                       KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, \
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_FN2)
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_LAYER_TAP_TOGGLE(1),
-  [1] = ACTION_LAYER_TAP_TOGGLE(2),
+  /* [0] = ACTION_LAYER_TAP_TOGGLE(1), */
+  /* [1] = ACTION_LAYER_TAP_TOGGLE(2), */
+  [0] = ACTION_LAYER_MOMENTARY(1),
+  [1] = ACTION_LAYER_MOMENTARY(2),
   [2] = ACTION_FUNCTION(BOOTLOADER),
   [3] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
   [4] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_ENT),
