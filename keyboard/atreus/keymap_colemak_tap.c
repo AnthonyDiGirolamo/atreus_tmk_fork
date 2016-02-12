c266a67582a8b5aa6404be4da9135fd511bb6e1b
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-02-12 18:21:53 -0500

    cleaned up indentation and comments

--- keyboard/atreus/keymap_colemak_tap.c
+++ keyboard/atreus/keymap_colemak_tap.c
@@ -1,42 +1,43 @@
 #include "keymap_common.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
   // 0: colemak
+  // Q  W  E  R  T      Y  U  I     O    P
+  // A  S  D  F  G      H  J  K     L    SCLN
+  // Z  X  C  V  B      N  M  COMM  DOT  SLSH
+
   KEYMAP(
-  /*  Q  W  E  R  T      Y  U  I     O    P     */
-  /*  A  S  D  F  G      H  J  K     L    SCLN  */
-  /*  Z  X  C  V  B      N  M  COMM  DOT  SLSH  */
-     KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,                        KC_J,     KC_L,    KC_U,     KC_Y,     KC_SCLN,  \
-     KC_A,     KC_R,     KC_S,     KC_T,     KC_D,                        KC_H,     KC_N,    KC_E,     KC_I,     KC_O,     \
-     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                        KC_K,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  \
-     KC_FN7,   KC_LGUI,   KC_TAB,   KC_FN1,   KC_FN6,  KC_FN3,   KC_FN4,   KC_FN5,   KC_FN0,  KC_MINS,  KC_QUOT,  KC_FN8), \
+         KC_Q,    KC_W,     KC_F,    KC_P,    KC_G,    /*       */       KC_J,    KC_L,    KC_U,     KC_Y,     KC_SCLN,  \
+         KC_A,    KC_R,     KC_S,    KC_T,    KC_D,    /*       */       KC_H,    KC_N,    KC_E,     KC_I,     KC_O,     \
+         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    /*       */       KC_K,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  \
+         KC_FN7,  KC_LGUI,  KC_TAB,  KC_FN1,  KC_FN6,  KC_FN3,  KC_FN4,  KC_FN5,  KC_FN0,  KC_MINS,  KC_QUOT,  KC_FN8),  \
 
   // 1: punctuation and numbers
-
   // *7890  |[]#!
   // +456~  @()^$
   // =123`  &{}%\
   //  0.      -'
 
   KEYMAP(
-    SHIFT(KC_8),   KC_7,    KC_8,    KC_9,    KC_0,            SHIFT(KC_BSLS), KC_LBRC,        KC_RBRC,        SHIFT(KC_3), SHIFT(KC_1),\
-    SHIFT(KC_EQL), KC_4,    KC_5,    KC_6,    SHIFT(KC_GRAVE), SHIFT(KC_2),    SHIFT(KC_9),    SHIFT(KC_0),    SHIFT(KC_6), SHIFT(KC_4),\
-    KC_EQL,        KC_1,    KC_2,    KC_3,    KC_GRAVE,        SHIFT(KC_7),    SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_5), KC_BSLS,    \
-    KC_TRNS,       KC_0,    KC_DOT,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   SHIFT(KC_MINS), SHIFT(KC_QUOT), KC_TRNS),\
+         SHIFT(KC_8),    KC_7,  KC_8,    KC_9,     KC_0,             /*        */        SHIFT(KC_BSLS),  KC_LBRC,         KC_RBRC,         SHIFT(KC_3),     SHIFT(KC_1), \
+         SHIFT(KC_EQL),  KC_4,  KC_5,    KC_6,     SHIFT(KC_GRAVE),  /*        */        SHIFT(KC_2),     SHIFT(KC_9),     SHIFT(KC_0),     SHIFT(KC_6),     SHIFT(KC_4), \
+         KC_EQL,         KC_1,  KC_2,    KC_3,     KC_GRAVE,         /*        */        SHIFT(KC_7),     SHIFT(KC_LBRC),  SHIFT(KC_RBRC),  SHIFT(KC_5),     KC_BSLS,     \
+         KC_TRNS,        KC_0,  KC_DOT,  KC_TRNS,  KC_TRNS,          KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,         SHIFT(KC_MINS),  SHIFT(KC_QUOT),  KC_TRNS),    \
 
   // 2: arrows and function keys
   KEYMAP(
-     KC_INS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,                       KC_HOME,  KC_RGHT,  KC_END,   KC_PGUP,  KC_PSCR, \
-     KC_DEL,   KC_F4,    KC_F5,    KC_F6,    KC_F11,                       KC_LEFT,  KC_DOWN,  KC_UP,    KC_PGDN,  KC_PAUSE, \
-     KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F12,                       KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_F13,   KC_F14, \
-     KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_FN2),
+         KC_INS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,   /*        */        KC_HOME,  KC_RGHT,  KC_END,   KC_PGUP,  KC_PSCR,   \
+         KC_DEL,   KC_F4,    KC_F5,    KC_F6,    KC_F11,   /*        */        KC_LEFT,  KC_DOWN,  KC_UP,    KC_PGDN,  KC_PAUSE,  \
+         KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F12,   /*        */        KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_F13,   KC_F14,    \
+         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_FN2),
 
   // 3: macros
   KEYMAP(
-  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    /*      */      KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  \
-  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    /*      */      KC_NO,  KC_NO,  KC_FN10,  KC_NO,  KC_NO,  \
-  KC_NO,    KC_NO,    KC_FN9,   KC_NO,    KC_NO,    /*      */      KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  \
-  KC_FN2,   KC_NO,    KC_NO,    KC_NO,    KC_BTN1,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_FN8)
+         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    /*      */      KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  \
+         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    /*      */      KC_NO,  KC_NO,  KC_FN10,  KC_NO,  KC_NO,  \
+         KC_NO,    KC_NO,    KC_FN9,   KC_NO,    KC_NO,    /*      */      KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  \
+         KC_FN2,   KC_NO,    KC_NO,    KC_NO,    KC_BTN1,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_FN8)
 };
 
 enum macro_id {
