fab0b54b951764f092bb5fe27da91abbd38551d5
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2016-02-13 09:38:03 -0500

    update build script for arch in virtualbox

--- keyboard/atreus/build.sh
+++ keyboard/atreus/build.sh
@@ -1,1 +1,7 @@
-KEYMAP=colemak_tap make clean && KEYMAP=colemak_tap make upload USB=/dev/cu.usbmodem1421
+if uname -a | grep 'arch'
+then
+	while [ 1 ] ; do sudo avrdude -p atmega32u4 -c avr109 -U flash:w:atreus.hex -P /dev/ttyACM0 ; done
+else
+	KEYMAP=colemak_tap make clean && KEYMAP=colemak_tap make upload USB=/dev/cu.usbmodem1421
+fi
+
